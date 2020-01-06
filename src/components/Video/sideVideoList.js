8302c3c654fc133f5cc1caee877cacf5cd71d53e
nadav-br <nadav.br@gmail.com>
2020-01-06 09:35:45 +0200

    inner video list

---
+++ src/components/Video/sideVideoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function sideVideoList() {
+    return (
+        <div className="sidevideolist">
+            
+        </div>
+    )
+}
+
+export default sideVideoList
