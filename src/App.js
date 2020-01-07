f191d2115176ac74e42a46bac4fcb5f69ce232a0
tt59317 <57324886+tt59317@users.noreply.github.com>
2020-01-07 20:19:01 +0200

    Create test.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import StoreProvider from './Store'
-import VideosList from './components/VideosList/VideosList'
-import Video from './components/Video/Video'
+import VideoReactionBar from "./Components/VideoReactionBar/VideoReactionBar";
 
 
 function App() {
   return (
     <>
-    <VideosList /> 
-    <Video />
+    <VideoReactionBar />
     </>
   );
 }
---
+++ src/components/DescripNcomments/DescripNcomments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VideoReactionBar from "../Components/VideoReactionBar/VideoReactionBar";
+
+function DescripNcomments() {
+  return (
+      <div className="DescripNcomments">
+        <VideoReactionBar />
+      </div>
+  );
+}
+
+export default DescripNcomments;
