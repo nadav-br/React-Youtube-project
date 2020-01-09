8ce91c92f1930dccfc71774fd8e931aacc515166
nadav-br <nadav.br@gmail.com>
2020-01-09 10:58:31 +0200

    basic server

--- src/server.js
+++ src/server.js
@@ -1,32 +1,15 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 
-const videosList = [{    
-    "id": "1",
-    "name": "Masha and the Bear",
-    "url": "https://www.youtube.com/watch?v=ac9a8mLgCHY"
+const videosList = [
+    { id: "1", name: "Masha and the Bear", url: "https://www.youtube.com/embed/ac9a8mLgCHY"},    
+    { id: "2", name: "ערוץ הופ!", url: "https://www.youtube.com/embed/tcMMmXpw6d8"},
+    { id: "3", name: "Disney Junior Israel", url: "https://www.youtube.com/embed/_SWySRlRKaw"},
+    { id: "4", name: "Disney Junior Israel", url: "https://www.youtube.com/embed/JWFdJHxVAKk"}
+]
 
-},    
-{
-     "id": "2",
-    "name": "ערוץ הופ!",
-    "url": "https://www.youtube.com/watch?v=tcMMmXpw6d8"
-},
-{
-    "id": "3",
-    "name": "Disney Junior Israel",
-    "url": "https://www.youtube.com/watch?v=XIepGJvUA7I"
-},
-{
-    "id": "4",
-    "name": "Disney Junior Israel",
-    "url": "https://www.youtube.com/watch?v=JWFdJHxVAKk"
-}]
-
-app.get('/', (req, res) => {
-    res.send(fs.readFileSync('index.js', { encoding: 'UTF-8' })
-    )
+app.get('/api/videosList', (req, res) => {    
+    res.json(videosList);    
 })
 
 app.listen(5000);
