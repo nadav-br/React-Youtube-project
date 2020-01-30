fd77fb1e58d484f63042094f04db4f2a982c9706
nadav-br <nadav.br@gmail.com>
2020-01-30 10:38:23 +0200

    app search update

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,15 +4,21 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import StoreProvider from "./components/Store/StoreProvider";
 import VideoPage from "./components/VideoPage/VideoPage";
 import HomePage from "./components/HomePage/HomePage";
+import SearchVideo from './components/SearchVideo/SearchVideo';
+import SearchResults from './components/SearchVideo/SearchResults/SearchResults'
+import logo from './youtube.png';
 
 
 const App = () => {
   return (
+    // <img src={logo} alt="logo" />
       <StoreProvider>
         <Router>
+        <SearchVideo />
           <Switch>
             <Route path="/" exact component={HomePage} />
             <Route path="/video-page/:id" exact component={VideoPage} />
+            <Route path="/search-results/:id" exact component={SearchResults} />
           </Switch>
         </Router>
       </StoreProvider>
@@ -20,3 +26,6 @@ const App = () => {
 }
 
 export default App;
+
+
+
