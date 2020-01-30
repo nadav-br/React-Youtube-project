8c31d91192c7f5952f7966ddad316e57c8cca7f9
nadav-br <nadav.br@gmail.com>
2020-01-30 11:17:22 +0200

    updated all

--- Client/src/components/HomePage/HomePage.js
+++ Client/src/components/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Feed from "./Feed/Feed";
-import SearchVideo from '../SearchVideo/SearchVideo';
+
 
 
 const HomePage = () => { 
@@ -8,7 +8,7 @@ const HomePage = () => {
     return (
       <div className='homePage'>      
         <Feed />
-        {/* <SearchVideo /> */}
+       
       </div>
     );
   
