dac22e279174a64e82151b16877616f24c7ce419
Shimesh <yaron.orb@gmail.com>
2020-02-06 13:02:37 +0200

    comments

--- Client/src/components/VideoPage/Comments/Comments.js
+++ Client/src/components/VideoPage/Comments/Comments.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Comments = () => {
-  return <div></div>;
+const Comments = ({ comments }) => {
+  const [newComment, setNewComment] = useState("");
+  const [c, setC] = useState();
+
+  const valInput = e => {
+    let input = e.target.value;
+    setNewComment(input);
+  };
+
+  const addComment = y => {
+    let added = newComment;
+    setNewComment(added);
+  };
+
+  return (
+    <div>
+      <h6>This is the Comments Section</h6>
+      <input
+        type="text"
+        placeholder="write a new comment..."
+        onChange={valInput}
+      ></input>
+      <button onClick={addComment}>Add Comment</button>
+
+      <ul>
+        {comments.map(comment => (
+          <li>
+            <h6>{newComment}</h6>
+            <h6>{comment.id}</h6>
+            <h6>{comment.first_name}</h6>
+            <h6>{comment.comment}</h6>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Comments;
