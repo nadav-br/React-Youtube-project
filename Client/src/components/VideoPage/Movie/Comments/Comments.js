26d4bd9e1428f8293c7e4a6105f7c2f1e851bfa9
Shimesh <yaron.orb@gmail.com>
2020-02-09 19:08:47 +0200

    unshift

--- Client/src/components/VideoPage/Movie/Comments/Comments.js
+++ Client/src/components/VideoPage/Movie/Comments/Comments.js
@@ -1,50 +1,45 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import _ from "lodash";
 import Comment from "./Comment";
 import Input from "./Input";
 
-const Comments = ({comments, id}) => {
-  
-const [comList,setComList] = useState([]);
+const Comments = ({ comments, id }) => {
+  const [comList, setComList] = useState([]);
 
-const addComment = (valueRef) => {
-  const newComment = {
-    user:"User name",
-    comment:valueRef,
-    id:id,
-    newCommentArr: [],
-  }
-  let filterComment = _.find(comList, { id:111 });
-  filterComment.newCommentArr.push(newComment)
-  // console.log(id);
-  // console.log(filterComment);
-  setComList([...comList]);
-}
-useEffect(() => {
-  setComList(comList)
-},[comList]);
+  const addComment = valueRef => {
+    const newComment = {
+      user: "User name",
+      comment: valueRef,
+      id: id,
+      newCommentArr: []
+    };
+    let filterComment = _.find(comList, { id: 111 });
+    filterComment.newCommentArr.unshift(newComment);
+    // console.log(id);
+    // console.log(filterComment);
+    setComList([...comList]);
+  };
+  useEffect(() => {
+    setComList(comList);
+  }, [comList]);
 
-useEffect(() => {
-  if(comments.length === 0){
-    return;
-  }
-  setComList(comments);
-},[comments]);
-console.log("B_comList",comList);
-
-
-
-
+  useEffect(() => {
+    if (comments.length === 0) {
+      return;
+    }
+    setComList(comments);
+  }, [comments]);
+  console.log("B_comList", comList);
 
   return (
     <div>
-      <Input addComment={addComment}/>
+      <Input addComment={addComment} />
       {comList.map(comment => {
-        return(
+        return (
           <div>
             <Comment comment={comment} addComment={addComment} id={id} />
           </div>
-        )
+        );
       })}
     </div>
   );
