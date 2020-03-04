1817d93f5284b2edb4a54f04420b00562e7748a5
nadav-br <nadav.br@gmail.com>
2020-03-04 12:02:10 +0200

    online server

--- Client/src/components/HomePage/Feed/Feed.js
+++ Client/src/components/HomePage/Feed/Feed.js
@@ -10,7 +10,7 @@ const Feed = () => {
   const [videoslist, setVideoList] = useState([]);
   
   useEffect(() => {
-    fetch("/videos")
+    fetch("/movies")
       .then(res => res.json())
       .then(videos => {
         setVideoList(videos);
