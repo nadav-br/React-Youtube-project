939eec95494cc16bf24ae696f03be7b4cc126339
tt59317 <57324886+tt59317@users.noreply.github.com>
2021-03-01 12:25:23 +0200

    Updates

---
+++ precache-manifest.ca6ac8295df03ad880e986b7f284f557.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc18a01d33a0bb36a2e25bdd621a5b1",
+    "url": "/nadav-br/React-Youtube-project/index.html"
+  },
+  {
+    "revision": "b5a5719857fce7cfb03d",
+    "url": "/nadav-br/React-Youtube-project/static/css/main.292cb616.chunk.css"
+  },
+  {
+    "revision": "e5394a153d43b9b8de0b",
+    "url": "/nadav-br/React-Youtube-project/static/js/2.c54414d6.chunk.js"
+  },
+  {
+    "revision": "96a3ca43f95d59d0e44e67bbe4d720ea",
+    "url": "/nadav-br/React-Youtube-project/static/js/2.c54414d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b5a5719857fce7cfb03d",
+    "url": "/nadav-br/React-Youtube-project/static/js/main.8a041cbe.chunk.js"
+  },
+  {
+    "revision": "aa2f07887674f1c05a7c",
+    "url": "/nadav-br/React-Youtube-project/static/js/runtime-main.7ac305ff.js"
+  },
+  {
+    "revision": "dae7aab67d18cb845a17d2f39d4d55ac",
+    "url": "/nadav-br/React-Youtube-project/static/media/YouTube_Logo.dae7aab6.svg"
+  },
+  {
+    "revision": "547bb2fac374cffa624de9c848888fbe",
+    "url": "/nadav-br/React-Youtube-project/static/media/clerk.547bb2fa.svg"
+  },
+  {
+    "revision": "54e2abf96af6654739796d299d85a4de",
+    "url": "/nadav-br/React-Youtube-project/static/media/dislike.54e2abf9.svg"
+  },
+  {
+    "revision": "80377f30564d6000993c926ba10c29d5",
+    "url": "/nadav-br/React-Youtube-project/static/media/like.80377f30.svg"
+  },
+  {
+    "revision": "b0faa13c2527673e2d8dbc068400f50c",
+    "url": "/nadav-br/React-Youtube-project/static/media/search.b0faa13c.svg"
+  }
+]);
